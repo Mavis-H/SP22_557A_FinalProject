d34d24dc6007b3fc49348f5bf191c3058dc2d7c7
Mavis-H <siqianhou990411@yahoo.com>
2022-04-21 15:23:46 -0500

    add store data module

--- scripts/MyMouseTrack.py
+++ scripts/MyMouseTrack.py
@@ -5,13 +5,15 @@ import cv2
 class MyMouseTrack:
 
     # store mouse movement data
-    def __init__(self, imageName):
+    def __init__(self, image_name):
         self.mouse_pos_per_second = []
-        self.imageName = imageName
+        # self.clicks = []
+        self.image_name = image_name
+        self.time_start = time.time()
 
     def start_track(self):
         # read and display the image
-        img = cv2.imread(self.imageName, 1)
+        img = cv2.imread(self.image_name, 1)
         cv2.imshow('image', img)
     
         # setting mouse handler for the image and calling the move_event() function
@@ -24,14 +26,17 @@ class MyMouseTrack:
         # close the window
         cv2.destroyAllWindows()
 
-    # function to track the position of mouse on the image per second
+    # function to track the position of mouse on the image the 
     def move_event(self, event, x, y, flags, params):
-
+        time_now = time.time()
+        time_past = round(time_now - self.time_start, 3) # past time since program start
         # checking for mouse movement
         if event == cv2.EVENT_MOUSEMOVE:
-            self.mouse_pos_per_second.append((x,y))
-        time.sleep(1)
+            self.mouse_pos_per_second.append(' '.join((str(x), str(y), str(time_past))))
+        
+        # if event == cv2.EVENT_LBUTTONDOWN:
+        #     self.clicks.append((x, y, time_past))
 
     # get mouse track data
-    def get_data(self):
+    def get_pos_data(self):
         return self.mouse_pos_per_second
--- scripts/main.py
+++ scripts/main.py
@@ -2,11 +2,26 @@ import os
 from MyMouseTrack import MyMouseTrack
 
 vis_dir = "../vis"
+data_dir = "../data"
 for filename in os.listdir(vis_dir):
     f = os.path.join(vis_dir, filename)
     # checking if it is a file
     # TODO: check if file is a valid VIS image depends on final vis format
-    if os.path.isfile(f) and filename.endswith(('.png', '.jpg', '.jpeg')):  
+    if os.path.isfile(f) and filename.endswith(('.png', '.jpg', '.jpeg')): 
         tracking = MyMouseTrack(f)
         tracking.start_track()
-        print(filename + ": " + tracking.get_data())
+
+        # store each vis data to its own dirctory
+        directory = os.path.join(data_dir, filename.split('.')[0])
+        if not os.path.isdir(directory):
+            os.mkdir(directory)
+
+        # count current result data file number
+        path, dirs, files = next(os.walk(directory))
+        file_count = len(files)
+
+        # write to txt file
+        txtfilepath = os.path.join(directory, str(file_count) + '.txt')
+        with open(txtfilepath, 'w') as f:
+            f.writelines('\n'.join(tracking.get_pos_data()))
+            f.close()
